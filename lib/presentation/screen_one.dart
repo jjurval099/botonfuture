5b2ae5a4a561079f0d27d0c922c4ae3840667068
jjurval099 <jjurval099@g.educaand.es>
2023-11-22 14:30:48 +0100

    first commit

---
+++ lib/presentation/screen_one.dart
@@ -0,0 +1,49 @@
+import 'package:botonfuture/presentation/screen_two.dart';
+import 'package:flutter/material.dart';
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({Key? key}) : super(key: key);
+
+  @override
+  _ScreenOneState createState() => _ScreenOneState();
+
+}
+
+
+class _ScreenOneState extends State<ScreenOne> {
+
+  bool _isLoading = false;
+
+  void _navigateToScreenTwo() {
+    setState(() {
+      _isLoading = true; 
+    });
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const ScreenTwo()),
+      );
+      setState(() {
+        _isLoading = false; 
+      });
+    }); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen One'),
+      ),
+      body: Center(
+        child: _isLoading
+            ? const CircularProgressIndicator() 
+            : ElevatedButton(
+                onPressed: _navigateToScreenTwo,
+                child: const Text('Ir a la Segunda Pantalla'),
+              ),
+      ),
+    );
+  }
+}
